9d8389992ba97a9ca02979a6992fb88ecad0eded
AlvishaWaghmare <78342228+AlvishaWaghmare@users.noreply.github.com>
2021-04-19 16:12:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var stone;
+var iron;
+var rubber;
+var options={
+    restitution:0.3,
+    friction:5,
+    density:1
+}
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    stone.display();
+    iron.display();;
+    rubber.display();
+
+
+    
+ 
+}
